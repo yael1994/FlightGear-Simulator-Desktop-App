031a5e5de04215b83257b527098311b5231771a5
daniel evelyne moshayof <danielevelyne.moshayof@live.biu.ac.il>
2019-04-11 12:36:56 +0300

    Manual2 the good one

---
+++ FlightSimulator/Views/Manual2.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FlightSimulator.Views
+{
+    /// <summary>
+    /// Interaction logic for Manual2.xaml
+    /// </summary>
+    public partial class Manual2 : UserControl
+    {
+        public Manual2()
+        {
+            InitializeComponent();
+        }
+
+        private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+
+        }
+
+        private void Slider_ValueChanged_1(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+
+        }
+
+        private void Joystick_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
